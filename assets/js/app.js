8c49955c58d7532aaabe96d20f39562b7d05be14
Bernhard Hofmann <bhofmann@hotmail.co.uk>
2012-03-15 22:23:17 +0000

    Fixing active menu on load and selection.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,26 @@
 function hideAllPages() {
 	$('.appPage').hide();
 }
+function setActiveMenu(menuId) {
+	$('.nav li').removeClass('active');
+	$('#'+ menuId).parent().addClass('active');
+}
 $('.nav li a').click(function() {
 	var menuId = this.id,
 	pageName = menuId.replace("menu",""),
 	pageId = pageName[0].toLowerCase() + pageName.substr(1) + 'Page';
 
-	console.log(pageId);
 	hideAllPages();
 	$('#'+ pageId).show();
+	setActiveMenu(menuId);
 });
 
 hideAllPages();
 if (window.location.hash.length <= 0) {
 	$('#homePage').show();
+	setActiveMenu('menuHome');
 } else {
-	$(window.location.hash + "Page").show();
+	$(window.location.hash + 'Page').show();
+	var menuId = window.location.hash.substr(1);
+	setActiveMenu('menu'+ menuId[0].toUpperCase() + menuId.substr(1));
 }
